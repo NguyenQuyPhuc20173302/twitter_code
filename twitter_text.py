c31af6a4984a35a8bec20460f9e94e84cf629908
RebornhHUST <58975613+RebornhHUST@users.noreply.github.com>
2020-06-22 20:48:09 +0700

    Add files via upload

---
+++ twitter_text.py
@@ -0,0 +1,7 @@
+import twitter_text
+# Sample usage
+txt = "RT @SocialWebMining Mining 1M+ Tweets About #Syria http://wp.me/p3QiJd-1I"
+ex = twitter_text.Extractor(txt)
+print "Screen Names:", ex.extract_mentioned_screen_names_with_indices()
+print "URLs:", ex.extract_urls_with_indices()
+print "Hashtags:", ex.extract_hashtags_with_indices()
